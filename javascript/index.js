4de15cd3aa540d6f422b3ced3d0044b1be5ff5c8
Ricardo Lopes <r.d.m.lopes@gmail.com>
2023-09-18 16:14:42 +0100

    refactor

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,4 @@
-import { ricardoImages, MainRicardoImage, ricardoResume } from 'javascript/data.js';
+import { ricardoImages, MainRicardoImage, ricardoResume } from './data.js';
 
 //Dom Elements-------------------------------------------------------------------------------------------------------------------------------------
 //Photo Elements
