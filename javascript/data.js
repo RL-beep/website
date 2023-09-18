3b7fc13218231f4db18a6b380f151c451987e50d
Ricardo Lopes <r.d.m.lopes@gmail.com>
2023-09-18 15:44:39 +0100

    refactor

--- javascript/data.js
+++ javascript/data.js
@@ -1,30 +1,30 @@
 export const ricardoImages = [
     {
-        image: "/images/front page/ricardo-fam.jpg",
+        image: "images/front page/ricardo-fam.jpg",
         alt: "Ricardo with some of his family"
     },
     {
-        image: "/images/front page/ricardo-mountain.jpg",
+        image: "images/front page/ricardo-mountain.jpg",
         alt: "Ricardo in front of some mountains" 
     },
     {
-        image: "/images/front page/ricardo-sheep.jpg",
+        image: "images/front page/ricardo-sheep.jpg",
         alt: "Ricardo with a sheep and his friends" 
     },
     {
-        image: "/images/front page/ricardo-team.jpg",
+        image: "images/front page/ricardo-team.jpg",
         alt: "Ricardo posing with a football team" 
     },
     {
-        image: "/images/front page/ricardo-wow.jpg",
+        image: "images/front page/ricardo-wow.jpg",
         alt: "Ricardo in a Ferris wheel" 
     },
     {
-        image: "/images/front page/ricardo-yun-red.jpg",
+        image: "images/front page/ricardo-yun-red.jpg",
         alt: "Ricardo with his Wife, Yun but in a red camera tint." 
     },
     {
-        image: "/images/front page/ricardo-field.jpg",
+        image: "images/front page/ricardo-field.jpg",
         alt: "Ricardo in a field "
     },
 
@@ -33,13 +33,13 @@ export const ricardoImages = [
 export const MainRicardoImage = 
     [
         {
-        image: "/images/front page/main image/ricardo-temple.jpg",
+        image: "images/front page/main image/ricardo-temple.jpg",
         alt: "Ricardo in front of a temple"
         }
     ]
 
     export const ricardoResume = [{
-        path: "/files/RL 2023.pdf",
+        path: "files/RL 2023.pdf",
         name:"Ricardo Lopes 2023.pdf",
         year: "2023"
     }]
